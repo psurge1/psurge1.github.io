4c67353bb5129b7f0fe607a801ee2556b849caf3
psurge1 <suraj.sswamy@gmail.com>
2026-09-04 20:46:42 -0500

    update personal blurb

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -16,7 +16,7 @@ export const portfolioData: PortfolioData = {
   profile: {
     name: 'Suraj Swamy',
     identity: 'Software Engineer',
-    description: "Hello! I’m Suraj Swamy. I’m studying Electrical and Computer Engineering at UT Austin. I am passionate about distributed systems, programming for performance, and ML systems. My favorite programming language is Go.\n\nI enjoy playing chess, watching football (nfl and cfb), and playing Apex Legends (I main Fuse) and EA College Football.\n\nI'm currently learning vim + tmux, and building my own distributed ML framework with support for CPUs, CUDA, and Metal",
+    description: "Hello! I’m Suraj Swamy. I’m studying Electrical and Computer Engineering at UT Austin. I am passionate about distributed systems, programming for performance, and ML systems. My favorite programming language is Go.\n\nI enjoy playing chess, watching football (nfl and cfb), and playing Apex Legends (I main Fuse) and EA College Football.\n\nI'm currently learning vim + tmux, and building my own distributed ML inference runtime with support for CPU, CUDA, and Metal",
   },
   education: [
     {
